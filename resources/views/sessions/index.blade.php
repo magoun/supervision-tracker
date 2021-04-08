632731aaf094b61ce6e3b95dbd97ea969645d048
Creighton Magoun <magoun@gmail.com>
2021-04-08 00:29:35 -0400

    Add action icons

--- resources/views/sessions/index.blade.php
+++ resources/views/sessions/index.blade.php
@@ -20,19 +20,30 @@
 
          <table class="table-auto border-collapse w-full">
             <thead>
-               <tr class="rounded-lg text-sm font-medium text-gray-700 text-left border-b border-gray-400" style="font-size: 0.9674rem">
-                  <th class="px-4 py-2 ">Date</th>
-                  <th class="px-4 py-2 ">Duration</th>
-                  <th class="px-4 py-2 ">Group?</th>
+               <tr class="rounded-lg text-sm font-medium text-gray-700 text-center border-b border-gray-400" style="font-size: 0.9674rem">
+                  <th class="px-2 py-2 ">Date</th>
+                  <th class="px-2 py-2 ">Duration</th>
+                  <th class="px-2 py-2 ">Group?</th>
+                  <th class="px-2 py-2 ">Actions</th>
                </tr>
             </thead>
             <tbody class="text-sm font-normal text-gray-700">
                
                @foreach ($sessions as $session)
-                  <tr class="hover:bg-gray-100 border-b border-gray-200 py-10">
-                     <td class="px-4 py-4">{{ $session->date }}</td>
-                     <td class="px-4 py-4">{{ $session->duration }}</td>
-                     <td class="px-4 py-4">{{ $session->isGroup }}</td>
+                  <tr class="hover:bg-gray-100 border-b text-center border-gray-200 py-10">
+                     <td class="px-2 py-4">{{ $session->date }}</td>
+                     <td class="px-2 py-4">{{ $session->duration }}</td>
+                     <td class="px-2 py-4">{{ $session->isGroup ? 'Yes' : 'No' }}</td>
+                     <td class="px-2 py-4">
+                        <div class="flex justify-evenly">
+                           <a href={{ route('sessions.edit', ['session' => $session->id]) }} class="text-green-600">
+                              <i class="fas fa-pencil-alt p-2"></i>
+                           </a>
+                           <a href="https://google.com" class="text-red-600">
+                              <i class="fas fa-trash-alt p-2"></i>
+                           </a>
+                        </div>
+                     </td>
                   </tr>
                @endforeach
                
